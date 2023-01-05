9c706ce81ef324ef5ef8133da6a511c381805742
yuriagopian <yuri.agopian@hotmail.com>
2023-01-05 13:53:39 -0300

    chore: creating cache controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+//importing Get, Inject, Inject, and CACHE_MANAGER from nestjs/common
+import { Controller, Get, Inject, CACHE_MANAGER } from '@nestjs/common';
+import Cache from 'cache-manager';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  //This would be our dummy database since we won't be connecting to a database in the article
+  randomNumDbs = Math.floor(Math.random() * 10);
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get-number-cache')
+  async getNumber(): Promise<any> {
+    // const teste = await this.cacheManager.
+    const val = await this.cacheManager.keys('number');
+    if (val) {
+      return {
+        data: val,
+        FromRedis: 'this is loaded from redis cache',
+      };
+    }
+
+    if (!val) {
+      await this.cacheManager.put('number', String(this.randomNumDbs));
+      return {
+        data: this.randomNumDbs,
+        FromRandomNumDbs: 'this is loaded from randomNumDbs',
+      };
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RedisClientOptions } from 'redis';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.registerAsync<RedisClientOptions>({
+      isGlobal: false,
+      useFactory: async () => ({
+        store: redisStore,
+        port: +process.env.REDIS_PORT,
+        host: process.env.REDIS_HOST,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
